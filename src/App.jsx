b7f90cdcaa12187b043a98476de38562b8ff4a34
Shivansh <shiv10ansh@gmail.com>
2023-12-01 21:00:08 +0530

    context api project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Login from './components/login'
+import Profile from './components/profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
